996077c8a9573599e45553bdecc653d678ca222a
Lucas Matias <101410443+MatiasDeLima@users.noreply.github.com>
2024-02-24 19:54:12 -0300

    practice week 1

--- Practice 4/main.py
+++ Practice 4/main.py
@@ -29,20 +29,19 @@
 # print(b); # ['Lucas', 'Matias', 'de', 'Lima']
 
 ## == Count string
-str = "P@#yn26at^&i5ve";
-
-chr = 0;
-dig = 0;
-spchr = 0;
+str = "P@#yn26at^&i5ve"
+chr = 0
+dig = 0
+spchr = 0
 
 for i in str:
     if i.isdigit():
-        dig = dig + 1;
+        dig = dig + 1
     elif i.isalpha():
-        chr = chr + 1;
+        chr = chr + 1
     else:
-        spchr = spchr + 1;
+        spchr = spchr + 1
 
-print(chr);
-print(dig);
-print(spchr);
+print(chr)
+print(dig)
+print(spchr)
